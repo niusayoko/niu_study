d164c88d49320303e773e4387d8b69dc5a7b8b85
niuqingsong <niuqingsng@jd.com>
2021-04-07 14:28:56 +0800

    静态内部类单例模式

---
+++ src/main/java/com/niu/study/designPattern/Singleton.java
@@ -0,0 +1,19 @@
+package com.niu.study.designPattern;
+
+public class Singleton {
+    //因为使用静态内部类，所以这里不需要定义成员变量；
+
+    //私有构造方法
+    private Singleton() {
+    }
+
+    //静态内部类，只有在调用getInstance方法时才加载类
+    //同时因为静态属性，只有在第一次加载类的时候初始化一 次，JVM帮我们保证了线程安全
+    private static class SingletonInstance {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    private static Singleton getInstance() {
+        return SingletonInstance.INSTANCE;
+    }
+}
